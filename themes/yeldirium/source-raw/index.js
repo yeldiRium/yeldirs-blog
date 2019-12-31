8d154febd10e58faf76b39528532bc6d913fad49
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2019-12-31 14:55:43 +0100

    Initial commit
    Contains basic hexo theme structure and configuration as well as demo
    content and some actual content.

---
+++ themes/yeldirium/source-raw/index.js
@@ -0,0 +1,10 @@
+/* style */
+import 'normalize-css/normalize.css';
+import '@fortawesome/fontawesome-free/js/all';
+
+import './scss/index.scss';
+
+/* script */
+import { insight } from './vendor/insight';
+
+insight(window.INSIGHT_CONFIG);
---
+++ themes/yeldirium/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: './source-raw/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'source')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  }
+};
