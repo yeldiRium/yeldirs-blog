acff8621bc3cd6afff410a0d89f9c8ab08b725b1
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-02-06 18:37:51 +0100

    chore: Reformat js.

--- themes/yeldirium/source-raw/js/mobileNavigation.js
+++ themes/yeldirium/source-raw/js/mobileNavigation.js
@@ -1,20 +1,24 @@
 const setupMobileNavigation = () => {
-  const navigationToggleButton = document.querySelector('#navigation-toggle-button');
-  const navigationWrapper = document.querySelector('.header__body');
+  const navigationToggleButton = document.querySelector(
+    "#navigation-toggle-button"
+  );
+  const navigationWrapper = document.querySelector(".header__body");
 
-  navigationToggleButton.addEventListener('click', () => {
-    const currentlyActive = navigationToggleButton.classList.contains('is-active');
+  navigationToggleButton.addEventListener("click", () => {
+    const currentlyActive = navigationToggleButton.classList.contains(
+      "is-active"
+    );
 
     if (currentlyActive) {
-      navigationToggleButton.classList.remove('is-active');
-      navigationWrapper.classList.remove('header__body--open');
+      navigationToggleButton.classList.remove("is-active");
+      navigationWrapper.classList.remove("header__body--open");
 
       return;
     }
 
-    navigationToggleButton.classList.add('is-active');
-    navigationWrapper.classList.add('header__body--open');
+    navigationToggleButton.classList.add("is-active");
+    navigationWrapper.classList.add("header__body--open");
   });
-}
+};
 
 export default setupMobileNavigation;
