a54c8edb4367e06cf7539e07b56bcefc6252385f
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-08-17 19:32:52 +0200

    feat: Minimize css.

--- themes/yeldirium/webpack.config.js
+++ themes/yeldirium/webpack.config.js
@@ -1,19 +1,24 @@
 const path = require("path");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./source-raw/index.js",
   output: {
     filename: "bundle.js",
-    path: path.join(__dirname, "source")
+    path: path.join(__dirname, "source"),
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
   },
   plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
@@ -24,10 +29,10 @@ module.exports = {
           {
             loader: "sass-loader",
             options: {
-              sourceMap: true
-            }
-          }
-        ]
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
@@ -36,14 +41,14 @@ module.exports = {
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
-              outputPath: "fonts/"
-            }
-          }
-        ]
-      }
-    ]
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
   },
   node: {
-    fs: "empty"
-  }
+    fs: "empty",
+  },
 };
